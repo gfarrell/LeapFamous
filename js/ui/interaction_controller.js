3b59eaa4d95f249706a7f2ae38231cdb05bdc81a
Gideon Farrell <me@gideonfarrell.co.uk>
2014-06-06 12:02:49 +0100

    InteractionController : add object registry and start interaction functionality
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- js/ui/interaction_controller.js
+++ js/ui/interaction_controller.js
@@ -1,37 +1,83 @@
 /* global define */
-define(['lodash', 'leap/hand'], function(_, Hand) {
-    'use strict';
+define(
+    [
+        'lodash',
+        'leap/hand',
+        'ui/interaction'
+    ],
+    function(_, Hand, Interaction) {
+        'use strict';
 
-    var InteractionController = function() {
-        this.hands = {};
-    };
+        var InteractionController = function() {
+            this.hands = {};
+            this.interactions = {};
+            this.objects = [];
+        };
 
-    _.extend(InteractionController.prototype, {
-        update: function(frame) {
-            var present = [];
-            _.each(frame.hands, function(hand) {
-                if(_.has(this.hands, hand.id)) {
-                    this.hands[hand.id].update(hand);
-                } else {
-                    this.hands[hand.id] = new Hand(hand);
+        _.extend(InteractionController.prototype, {
+            registerObject: function(obj) {
+                if(this.objects.indexOf(obj) == -1) {
+                    return this.objects.push(obj);
                 }
+            },
 
-                present.push(hand.id);
-            }.bind(this));
+            unregisterObject: function(obj) {
+                var i = this.objects.indexOf(obj);
+                delete this.objects[i];
+                this.objects = _.compact(this.objects);
+            },
 
-            this.pruneHandsNotInList(present);
-        },
+            update: function(frame) {
+                var present = [];
+                _.each(frame.hands, function(hand) {
+                    if(_.has(this.hands, hand.id)) {
+                        this.hands[hand.id].update(hand);
+                    } else {
+                        this.hands[hand.id] = new Hand(hand);
+                    }
 
-        pruneHandsNotInList: function(list) {
-            var ourHands = _.keys(this.hands);
-            var pruneList = _.difference(ourHands, list);
+                    present.push(hand.id);
+                }.bind(this));
 
-            _.each(pruneList, function(id) {
-                this.hands[id].destroy();
-                delete this.hands[id];
-            }.bind(this));
-        }
-    });
+                this.pruneHandsNotInList(present);
 
-    return InteractionController;
-});
+                this.checkInteractions();
+            },
+
+            pruneHandsNotInList: function(list) {
+                var ourHands = _.keys(this.hands);
+                var pruneList = _.difference(ourHands, list);
+
+                _.each(pruneList, function(id) {
+                    this.hands[id].destroy();
+                    delete this.hands[id];
+                }.bind(this));
+            },
+
+            checkInteractions: function() {
+                // Need to see if our hands are grabbing
+                // If hand.isGrabbing() and no interaction exists, start one
+                // If hand.isGrabbing() and interaction exists, then ignore
+                // If not hand.isGrabbing() and interaction exists, destroy interaction
+                // If not hand.isGrabbing() and no interaction exists, ignore
+
+                _.each(this.hands, function(hand, id) {
+                    var grab = hand.isGrabbing();
+                    var haveInteraction = _.has(this.interactions, id);
+
+                    if(!grab && haveInteraction) {
+                        this.interactions[id].destroy();
+                        delete this.interactions[id];
+                    }
+
+                    if(grab && !haveInteraction) {
+                        // Create interaction
+                        // Store interaction
+                    }
+                }.bind(this));
+            }
+        });
+
+        return InteractionController;
+    }
+);
