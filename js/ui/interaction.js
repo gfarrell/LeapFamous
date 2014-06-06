2cd1d82a081a71a1f1baf968b57af83667373169
Gideon Farrell <me@gideonfarrell.co.uk>
2014-06-06 17:45:03 +0100

    ui/interaction.js : moveWithHand()
    Added hand movement ~ object movement correlation
    
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- js/ui/interaction.js
+++ js/ui/interaction.js
@@ -1,16 +1,27 @@
 /* global define */
-define(['lodash'], function() {
+define(['lodash', 'registry', 'leap/hand'], function(_, Registry, Hand) {
     'use strict';
 
     var Interaction = function(hand, obj) {
         this.hand = hand;
         this.object = obj;
+
+        this.hand.bind('update', this.moveWithHand.bind(this));
     };
 
     _.extend(Interaction.prototype, {
         destroy: function() {
+            // Unbind events
+            this.hand.unbind('update', this.moveWithHand.bind(this));
+
+            // Remove refs
             delete this.hand;
             delete this.object;
+        },
+
+        moveWithHand: function() {
+            var newPos = Hand.normaliseCoordinatesToContext(this.hand.getPosition(), Registry.get('FamousContext')._size);
+            this.object.moveTo(newPos, true);
         }
     });
 
