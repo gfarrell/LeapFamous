4e4264f534a924f56075e7b45c30a500931a22c4
Gideon Farrell <me@gideonfarrell.co.uk>
2014-06-07 16:38:57 +0100

    main.js
    Brought in managers
    
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- js/main.js
+++ js/main.js
@@ -21,20 +21,31 @@ require(
         'leapjs',
         'famous/core/Engine',
         'registry',
+        'ui/trackers_manager', 'ui/object_manager',
         'ui/interaction_controller',
         'test/board'
     ],
-    function(Leap, Engine, Registry, InteractionController, TestBoard) {
+    function(Leap, Engine, Registry, HandTrackerManager, UIObjectManager, InteractionController, TestBoard) {
         var controllerOptions = {
                 enableGestures: true
         };
 
+        // Famous initiation
         var context = Engine.createContext();
 
+        // Managers
+        var HTM = new HandTrackerManager();
+        var UOM = new UIObjectManager();
+
+        // Interaction Controller
         var IC = new InteractionController();
 
-        Registry.register('InteractionController', IC);
+        // Register relevant things in the Registry
         Registry.register('FamousContext', context);
+        Registry.register('HandTrackerManager', HTM);
+        Registry.register('UIObjectManager', UOM);
+        Registry.register('InteractionController', IC);
+
 
         Leap.loop(controllerOptions, function(frame) {
             IC.update(frame);
