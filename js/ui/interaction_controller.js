1b9c6edd91d97ff7aa33637e061f0b22c08ebd85
Gideon Farrell <me@gideonfarrell.co.uk>
2014-06-08 01:40:10 +0100

    ui/interaction_controller
    Removed the functionality that has been abstracted away into other classes as appropriate.
    
    Separation of concerns and all that.
    
    Signed-off-by: Gideon Farrell <me@gideonfarrell.co.uk>

--- js/ui/interaction_controller.js
+++ js/ui/interaction_controller.js
@@ -4,103 +4,45 @@ define(
         'lodash',
         'registry',
         'leap/hand',
-        'ui/interaction'
+        'ui/interaction',
+        'ui/hand_tracker'
     ],
-    function(_, Registry, Hand, Interaction) {
+    function(_, Registry, Hand, Interaction, HandTracker) {
         'use strict';
 
         var InteractionController = function() {
-            this.hands = {};
             this.interactions = {};
-            this.objects = [];
+
+            Registry.subscribe('HandUpdated', this.checkInteractions.bind(this));
         };
 
         _.extend(InteractionController.prototype, {
-            registerObject: function(obj) {
-                if(this.objects.indexOf(obj) == -1) {
-                    return this.objects.push(obj);
-                }
-            },
-
-            unregisterObject: function(obj) {
-                var i = this.objects.indexOf(obj);
-                delete this.objects[i];
-                this.objects = _.compact(this.objects);
-            },
-
-            update: function(frame) {
-                var present = [];
-                _.each(frame.hands, function(hand) {
-                    if(_.has(this.hands, ''+hand.id)) {
-                        this.hands[hand.id].update(hand);
-                    } else {
-                        this.hands[hand.id] = new Hand(hand);
-                    }
-                    this.hands[hand.id].setPosition(frame.interactionBox.normalizePoint(hand.stabilizedPalmPosition, true));
-                    present.push(hand.id);
-                }.bind(this));
-
-                this.pruneHandsNotInList(present);
-
-                this.checkInteractions();
-            },
-
-            pruneHandsNotInList: function(list) {
-                var ourHands = _.keys(this.hands);
-
-                _.each(ourHands, function(id) {
-                    if(list.indexOf(parseInt(id)) == -1) {
-                        this.hands[id].destroy();
-                        delete this.hands[id];
-                    }
-                }.bind(this));
-            },
-
-            objectAt: function(point) {
-                // Cycle through objects
-                // Find which one is under this point
-
-                var objectsAtPosition = [];
-
-                _.each(this.objects, function(obj) {
-                    if(obj.containsPoint(point)) {
-                        objectsAtPosition.push(obj);
-                    }
-                });
-
-                // At the moment we're just returning the last object in the list
-                // This needs to be improved with some awareness of z-ordering
-                return objectsAtPosition.length > 0 ? _.last(objectsAtPosition) : null;
-            },
-
-            checkInteractions: function() {
+            checkInteractions: function(id, hand) {
                 // Need to see if our hands are grabbing
                 // If hand.isGrabbing() and no interaction exists, start one
                 // If hand.isGrabbing() and interaction exists, then ignore
                 // If not hand.isGrabbing() and interaction exists, destroy interaction
                 // If not hand.isGrabbing() and no interaction exists, ignore
 
-                _.each(this.hands, function(hand, id) {
-                    var grab = hand.isGrabbing();
-                    var haveInteraction = _.has(this.interactions, id);
+                var grab = hand.isGrabbing();
+                var haveInteraction = _.has(this.interactions, id);
 
-                    if(!grab && haveInteraction) {
-                        this.interactions[id].destroy();
-                        delete this.interactions[id];
+                if(!grab && haveInteraction) {
+                    this.interactions[id].destroy();
+                    delete this.interactions[id];
+                }
+
+                if(grab && !haveInteraction) {
+                    // Create interaction
+                    // Store interaction
+                    var handPosNorm = Hand.normaliseCoordinatesToContext(hand.getPosition(), this.contextSize());
+                    var objAtPosn = Registry.get('UIObjectManager').objectAt(handPosNorm);
+
+                    if(objAtPosn !== null) {
+                        var ia = new Interaction(hand, objAtPosn);
+                        this.interactions[id] = ia;
                     }
-
-                    if(grab && !haveInteraction) {
-                        // Create interaction
-                        // Store interaction
-                        var handPosNorm = Hand.normaliseCoordinatesToContext(hand.getPosition(), this.contextSize());
-                        var objAtPosn = this.objectAt(handPosNorm);
-
-                        if(objAtPosn !== null) {
-                            var ia = new Interaction(hand, objAtPosn);
-                            this.interactions[id] = ia;
-                        }
-                    }
-                }.bind(this));
+                }
             },
 
             contextSize: function() {
